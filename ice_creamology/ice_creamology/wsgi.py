b22a7e1f8cf6613193705895e695db9e4db52eaf
m-mukhanova <35366943+m-mukhanova@users.noreply.github.com>
2022-11-17 17:19:26 +0300

    update names

--- ice_creamology/ice_creamology/wsgi.py
+++ ice_creamology/ice_creamology/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for icecreamology project.
+WSGI config for ice_creamology project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icecreamology.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ice_creamology.settings')
 
 application = get_wsgi_application()
--- ice_creamology/manage.py
+++ ice_creamology/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icecreamology.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ice_creamology.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
